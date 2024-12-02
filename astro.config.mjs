3378e6ae2c43ad3e565f72e8519bf68f32cef10d
Patrick White <you@example.com>
2024-12-02 14:17:33 -0500

    added tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://regal-tarsier-8b60f7.netlify.app/"
+});
